d810759b13ec56a825831d0faa7439d7eeaa5065
ale <aledeum.saf@gmail.com>
2024-06-13 18:15:14 -0300

    ..

--- setup.sh
+++ setup.sh
@@ -5,41 +5,71 @@ read something
 
 apt update
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 apt upgrade
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 apt install -y git
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 apt install -y gh
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 apt install -y curl
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 apt install -y htop
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 apt install -y thefuck
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 apt install -y zoxide
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
-apt install -y cargo
+echo ""
 sleep 10
+apt install stow
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 # Librewolf
 apt update && apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
 
@@ -55,11 +85,17 @@ Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
 
 apt update
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 apt install librewolf -y
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 
 # Nvim
@@ -67,46 +103,72 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage /usr/local/bin/nvim
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
-
-git clone -o ~/config/nvim https://github.com/CoolRobotsAndStuff/nvim-config.git
-
+echo ""
 sleep 10
+
+git clone https://github.com/CoolRobotsAndStuff/nvim-config.git
+mv ./nvim-config ~/.config/nvim/
+
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 # Config files
-git clone -o ~/dotfiles https://github.com/CoolRobotsAndStuff/dotfiles.git
+git clone  https://github.com/CoolRobotsAndStuff/dotfiles.git
+mv ./dotfiles/ ~/dotfiles/
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 stow ~/dotfiles/
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 # Terminal
 cargo install alacritty
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 # i3wm
 cargo install easy-i3-neovim-nav
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 apt install -y i3
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 apt install -y picom
 
-sleep 10
+echo ""
+echo ""
 echo "Letting pen drive rest..."
+echo ""
+sleep 10
 
 
 echo "Now you need to update the .xinitrc, and install the FiraCodeNerdFont."
